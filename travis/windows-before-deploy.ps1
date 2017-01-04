ea2665dd685dbf8625b49490812182fc2f5ea7de
Ron Hashimoto <mail@h2so5.net>
2017-01-05 01:05:52 +0900

    workaround for a riot.js bug on win32

--- travis/windows-before-deploy.ps1
+++ travis/windows-before-deploy.ps1
@@ -3,6 +3,8 @@ if($env:APPVEYOR_REPO_TAG_NAME -ne $null){
   $env:NO_WPCAP = ""
   yarn
   gulp build
+  # workaround for https://github.com/dripcap/dripcap/issues/15
+  wget https://raw.githubusercontent.com/Swaven/riot/cfb3bb2d6da73059f06b0379a6f91bd32f350935/lib/server/index.js -OutFile .build\node_modules\riot\lib\server\index.js
   gulp out
   gulp win32
   mv .builtapp\Dripcap-win32-x64 .builtapp\Dripcap
